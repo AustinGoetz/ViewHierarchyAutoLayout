e4897ab5b49d3f75c7d70b9acb8fb4a04ded8370
Austin Goetz <a.goetz09@gmail.com>
2019-12-16 12:38:28 -0700

    adds simple colored subviews

---
+++ ViewHierarchyAutoLayout/Controllers/TestViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TestViewController.swift
+//  ViewHierarchyAutoLayout
+//
+//  Created by Austin Goetz on 12/16/19.
+//  Copyright © 2019 Austin Goetz. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Adding views programatically
+            // X is how far down from the top corner we want to go
+            // Y is how far right from the top corner we want to go
+        let redView = UIView(frame: CGRect(x: 20, y: 20, width: 150, height: 150))
+        
+        // Set the view background color
+        redView.backgroundColor = .red
+        
+        // Add the created view as a subview of the superview
+        view.addSubview(redView)
+        
+        let blueView = UIView(frame: CGRect(x: 20, y: 20, width: 100, height: 100))
+        blueView.backgroundColor = .blue
+        
+        // Add the created blueView as a subview of the redView
+        redView.addSubview(blueView)
+    }
+}
